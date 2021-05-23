77e98446e8a6fdbb8442666751d9e1b2eea08bd1
Hitansh Kushwaha <77332460+hitanshkushwaha@users.noreply.github.com>
2021-05-23 14:11:37 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,31 @@
+class BAll{
+    constructor(){
+        var options={
+            restitution:0.4
+        }
+       this.r=r
+       this.body = Bodies.circle(x,y,this.r,options);
+       this.color=color(random(0,225),random(0,225),random(0,225));
+       World.add(this.body,world);
+    
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        Push();
+        translate(pos.x,pos.y)
+        rotate(angle);
+        noStroke();
+        fill(this.color);
+        ellipseMode(CENTER);
+        ellipse(0,0,this.r,this.r);
+        Pop();
+        
+
+ 
+    }
+
+    
+
+}
